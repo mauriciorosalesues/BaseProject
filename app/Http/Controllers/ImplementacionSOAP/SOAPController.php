5943d5ccab9b5034ec27c851f218a8bb6b7af9b7
krlos-514a <krlos.514a@gmail.com>
2025-04-29 14:15:36 -0600

    Actualizar SOAPController.php
    Se agregan comentarios al archivo SOAPController

--- app/Http/Controllers/ImplementacionSOAP/SOAPController.php
+++ app/Http/Controllers/ImplementacionSOAP/SOAPController.php
@@ -9,11 +9,14 @@ use SoapFault;
 
 class SOAPController extends Controller
 {
+    //Esta función dirige a la vista donde se capturaran los datos para la operacion aritmética (Suma o multiplicacion)
     public function capturarSolicitud()
     {
+        //Dentro del view se debe colocar el nombre de la vista para llamarla
         return view('VistaSolicitudAqui');
     }
 
+    //Esta función utiliza los datos capturados para realizar la operacion aritmética (suma o multiplicación, al final envía a la vosta donde se muestra em resultado
     public function procesarSolicitud(Request $request)
     {
         $num1 = $request->input('num1');
@@ -45,6 +48,7 @@ class SOAPController extends Controller
             $error = 'Error al conectar con el servicio SOAP: ' . $e->getMessage();
         }
 
+        //En el view se debe colocar el nombre de la vista donde se muestra el rwsultado, para llamarla
         return view('VistaResultadoAqui', compact('num1', 'num2', 'operacion', 'resultado', 'error'));
     }
 }
