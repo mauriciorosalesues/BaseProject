d6344cc42c676d064cd81d7292911365ee96aa8f
Ricardo24Heredia <hr21024@ues.edu.sv>
2025-04-29 20:03:58 -0600

    feat: implementar controlador y vista para servicio SOAP

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,8 @@ use App\Http\Controllers\Backend\Roles\PermisoController;
 use App\Http\Controllers\Backend\Perfil\PerfilController;
 use App\Http\Controllers\Backend\Configuracion\ConfiguracionController;
 use App\Http\Controllers\Backend\Registro\RegistroController;
-
+use App\Http\Controllers\SoapController;
+use App\Http\Controllers\CalculatorController;
 
 
 use App\Http\Controllers\Backend\Dashboard\DashboardController;
@@ -57,4 +58,7 @@ Route::get('sin-permisos', [ControlController::class,'indexSinPermiso'])->name('
 
 Route::get('/admin/dashboard', [DashboardController::class,'vistaDashboard'])->name('admin.dashboard.index');
 
+//  SOAP
+Route::get('/soap', [SoapController::class, 'showForm']);
+Route::post('/soap', [SoapController::class, 'processForm']);
 
