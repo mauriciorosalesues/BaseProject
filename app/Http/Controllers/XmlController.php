eba6e40e6de27e32937117c0d9d3c40ffe994b61
luispolanco <luispolanco@luispolanco.168.1.8>
2025-04-30 01:39:41 -0600

    Controlador con lectura de XML, conversión a JSON y envío a vista

---
+++ app/Http/Controllers/XmlController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class XmlController extends Controller
+{
+    public function mostrar()
+    {
+        $xmlPath = storage_path('xml/libros.xml');
+
+        if (!file_exists($xmlPath)) {
+            return response()->json(['error' => 'Archivo XML no encontrado.'], 404);
+        }
+
+        $xml = simplexml_load_file($xmlPath);
+
+        $libros = [];
+
+        foreach ($xml->libro as $libro) {
+            $libros[] = [
+                'titulo' => (string) $libro->titulo,
+                'autor' => (string) $libro->autor,
+                'anio' => (int) $libro->anio,
+            ];
+        }
+
+        // Convertimos a JSON para cumplir con el requisito
+        $jsonLibros = json_encode($libros);
+
+        // Enviamos el JSON decodificado a la vista
+        return view('preferencias', ['libros' => json_decode($jsonLibros)]);
+    }
+}
+
