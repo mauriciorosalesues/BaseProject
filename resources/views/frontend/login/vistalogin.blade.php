301319daa985eef6342b24d4de18065ce16ecf37
Ricardo24Heredia <hr21024@ues.edu.sv>
2025-04-30 08:25:16 -0600

    refactor(login): actualiza rutas de imágenes en la vista

--- resources/views/frontend/login/vistalogin.blade.php
+++ resources/views/frontend/login/vistalogin.blade.php
@@ -2,13 +2,13 @@
 <html lang="es">
 
 <head>
-    <title>Panel</title>
+    <title>Registro de Vehiculos</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{ asset('css/login/bootstrap.min.css') }}">
 
     <!-- icono del sistema -->
-    <link href="{{ asset('images/icono-sistemalogo.png') }}" rel="icon">
+    <link href="{{ asset('images/Logo_carros.png') }}" rel="icon">
     <!-- libreria -->
     <link href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}" type="text/css" rel="stylesheet" />
 
@@ -22,11 +22,11 @@
 
     <style>
         html, body {
-            height: 100%;
+            height: 95%;
         }
         body {
             font-family: 'Roboto', sans-serif;
-            background-image: url({{ asset('images/fondo3.jpg') }});
+            background-image: url({{ asset('images/Fondo_carro.png') }});
         }
 
         .demo-container {
@@ -75,10 +75,10 @@
 
                         <div class="p-5 bg-white rounded shadow-lg">
                             <div class="text-center image-size-small position-relative">
-                                <img src="{{ asset('images/logo.png') }}" class=" p-2">
+                                <img src="{{ asset('images/Logo_carros.png') }}" class=" p-2">
                             </div>
                             <h3 class="mb-2 text-center pt-5"><strong>&nbsp;</strong></h3>
-                            <p class="text-center lead" style="font-weight: bold">BASE</p>
+                            <p class="text-center lead" style="font-weight: bold">Registro de Vehiculos</p>
                             <form>
                                 <label style="margin-top: 10px" class="font-500">Usuario</label>
                                 <input class="form-control form-control-lg mb-3" id="usuario" autocomplete="off" type="text">
